a6580adbe20dfe13eae30e3298818acf10aefbde
ginpay <y-toyoda@live.jp>
2022-08-16 11:47:13 +0900

    プロジェクト ファイルを追加します。

---
+++ GinpayFactory/Commands/MyCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace GinpayFactory
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("GinpayFactory", "Button clicked");
+        }
+    }
+}
