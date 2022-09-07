a69343118cc582a96b1c762bb93f9cf269ebd5e4
toyoda-divakk <79815702+toyoda-divakk@users.noreply.github.com>
2022-09-07 18:50:45 +0900

    プロジェクトの取得

--- GinpayFactory/Commands/MyCommand.cs
+++ GinpayFactory/Commands/MyCommand.cs
@@ -1,11 +1,32 @@
-﻿namespace GinpayFactory
+﻿using Microsoft.VisualStudio.Text;
+using System.Linq;
+
+namespace GinpayFactory
 {
     [Command(PackageIds.MyCommand)]
     internal sealed class MyCommand : BaseCommand<MyCommand>
     {
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            await VS.MessageBox.ShowWarningAsync("GinpayFactory", "Button clicked");
+            // アクティブなテキスト ビューを取得する
+            DocumentView docView = await VS.Documents.GetActiveDocumentViewAsync();
+            if (docView?.TextView == null) return; //not a text window
+            SnapshotPoint position = docView.TextView.Caret.Position.BufferPosition;
+            docView.TextBuffer?.Insert(position, "some text"); // Inserts text at the caret
+
+            //// 含まれているファイルからプロジェクトを取得する
+            //string fileName = "c:\\file\\in\\project.txt";
+            //PhysicalFile item = await PhysicalFile.FromFileAsync(fileName);
+            //Project project = item.ContainingProject;
+
+            // ソリューション内のすべてのプロジェクトを取得する
+            var projects = await VS.Solutions.GetAllProjectsAsync();
+            var project = projects.First();
+            var references = project.References;
+
+            // プロジェクトは取れるけど、名前の取得とか、ファイルの追加とかぐらいしか出来ないっぽい。
+
+            Console.WriteLine();
         }
     }
 }
