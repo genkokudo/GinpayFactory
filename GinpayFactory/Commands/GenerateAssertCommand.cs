13a4b0f4700109e3865e880443be52a95058f24b
ginpay <y-toyoda@live.jp>
2022-10-04 18:55:24 +0900

    どうやって置換するのか？

--- GinpayFactory/Commands/GenerateAssertCommand.cs
+++ GinpayFactory/Commands/GenerateAssertCommand.cs
@@ -18,31 +18,43 @@ namespace GinpayFactory
             // 各コマンドの最初に必要
             await Package.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-            // DI
-            var assert = Ioc.Default.GetService<IAssertService>();
-
-            Assembly assem = Assembly.GetExecutingAssembly();
-            //Assembly assem2 = Assembly.GetExecutingAssembly();
-            //Assembly assem3 = Assembly.GetExecutingAssembly();
-
-            // TODO:現在表示中のアセンブリ名を取得
-            // TODO:現在表示中のクラス名を指定
-            //var targetObject = assem.CreateInstance("StudyRoslyn.Sample");
-
-            await VS.MessageBox.ShowWarningAsync($"{assem.FullName}", "Button clicked");
-            //await VS.MessageBox.ShowWarningAsync($"{assem2.FullName}", "Button clicked");
-            //await VS.MessageBox.ShowWarningAsync($"{assem3.FullName}", "Button clicked");
-
-            //Console.WriteLine(new AssertService().MakeAssert(targetObject, nameof(targetObject)));
-
-            //if (!string.IsNullOrWhiteSpace(result))
-            //{
+            // TODO:csが出来たとして、どうやって既存コードと置換するのか？
+            // Delete,Insertはできるので、あとはDeleteする範囲を取得できればよい。それはRoslynで取れるはず。
+            
             //    // 成功したら、カーソルの所に挿入
             //    var docView = await VS.Documents.GetActiveDocumentViewAsync();
             //    var selection = docView.TextView.Selection;
             //    docView.TextBuffer.Delete(selection.SelectedSpans[0].Span);
             //    docView.TextBuffer.Insert(selection.Start.Position, result);
-            //}
+
+
+            #region 取り敢えず置いといて
+            //// DI
+            //var assert = Ioc.Default.GetService<IAssertService>();
+
+            //Assembly assem = Assembly.GetExecutingAssembly();
+            ////Assembly assem2 = Assembly.GetExecutingAssembly();
+            ////Assembly assem3 = Assembly.GetExecutingAssembly();
+
+            //// TODO:現在表示中のアセンブリ名を取得
+            //// TODO:現在表示中のクラス名を指定
+            ////var targetObject = assem.CreateInstance("StudyRoslyn.Sample");
+
+            //await VS.MessageBox.ShowWarningAsync($"{assem.FullName}", "Button clicked");
+            ////await VS.MessageBox.ShowWarningAsync($"{assem2.FullName}", "Button clicked");
+            ////await VS.MessageBox.ShowWarningAsync($"{assem3.FullName}", "Button clicked");
+
+            ////Console.WriteLine(new AssertService().MakeAssert(targetObject, nameof(targetObject)));
+
+            ////if (!string.IsNullOrWhiteSpace(result))
+            ////{
+            ////    // 成功したら、カーソルの所に挿入
+            ////    var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            ////    var selection = docView.TextView.Selection;
+            ////    docView.TextBuffer.Delete(selection.SelectedSpans[0].Span);
+            ////    docView.TextBuffer.Insert(selection.Start.Position, result);
+            ////}
+            #endregion
         }
     }
 }
